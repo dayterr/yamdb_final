be98f5592393c24b5af27046c1ce688867d4c0a7
Mariya Dayter <mvdayter@gmail.com>
2021-10-22 22:39:53 +0300

    deploy

--- yamdb/permissions.py
+++ yamdb/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
-from rest_framework.permissions import BasePermission
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly
 
 
 class IsAdminOrReadOnly(BasePermission):
