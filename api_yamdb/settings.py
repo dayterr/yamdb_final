82c47727d86abdcf00f52dd8d729bc8085b69eeb
Mariya Dayter <mvdayter@gmail.com>
2021-10-24 01:50:36 +0300

    deploy2

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('KEY'),
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
