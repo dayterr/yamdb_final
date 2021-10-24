6c75d303cc69959613ee76425d5063c6f65d6e3a
Mariya Dayter <mvdayter@gmail.com>
2021-10-24 03:36:13 +0300

    deploy2

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['*']).split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
