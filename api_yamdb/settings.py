7eb39e07b336de670575e1974d523fe4836b8f1c
Mariya Dayter <mvdayter@gmail.com>
2021-10-24 03:32:08 +0300

    deploy2

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -2,9 +2,6 @@ import os
 from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
 
-env = environ.Env()
-environ.Env.read_env()
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
